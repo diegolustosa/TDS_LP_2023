27e15449f6d66940bda415a170ccac0cabad4ec4
Diego Alex <diegolustosasantos@gmail.com>
2023-11-22 21:13:27 -0300

    meu primeiro commit

---
+++ Exercicio_01/exercicio_01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+    int main() {
+
+    int num;
+    printf("Digite aqui um numero");
+    scanf("%d", num);
+
+    if(num % 2 == 0) {
+
+        printf("Seu numero e' par");
+    }
+    else {
+
+        printf("seu numero e' impar");
+    }
+
+    return 0;
+    }
