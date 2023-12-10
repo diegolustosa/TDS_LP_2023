3bcaf975978cb6cbaab16c0d610e147499eebc26
Diego Alex <diegolustosasantos@gmail.com>
2023-12-10 09:47:48 -0300

    Atualizando atividades

--- Atividades_Senai/ListaexercVETOR/exerc3.c
+++ Atividades_Senai/ListaexercVETOR/exerc3.c
@@ -2,21 +2,18 @@
 
 
     int main() {
-      int vetor[10];
-      int valorResultado = 0;
-
-    for(int i = 0; i < 10; i++) {
-        printf("Digite o valor da posicao do numero");
-        scanf("%d", &vetor[i]);
-        if(vetor[i] % 2 == 0) {
-            printf("Seu resultado e' par");
-            
+      int pares = 0;
+      int vet[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+      
+      for(int i = 0; i < 10; i++){
+        printf("Digite um valor para posicao");
+        scanf("%d", &vet[i]);
+      }
+      for(int i = 0; i < 10; i++) {
+        if(vet[i] % 2 == 0) {
+          pares++;
         }
-    }
-    else if(vetor[i] % 2 != 0) {
-        printf("Seu resultado e' impar");
-    }
-
-    
-    return 0;
-    }
+      }
+      printf("A quantidade de numeros pares e': %d", pares);
+      return 0;
+    } 
