afb8662a81a87e91f4a20588397c22ba1825dd33
Diego Alex <diegolustosasantos@gmail.com>
2023-12-13 19:17:52 -0300

    Atualizacao

--- Atividades_Senai/ListaexercVETOR/exerc4gaba.c
+++ Atividades_Senai/ListaexercVETOR/exerc4gaba.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 
-    int main(){
-    
-    int vetor[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    for(int i = 0; i < 10; i++) {
-    printf("Digite uma posicao para o vetor");
-    scanf("%d", &vetor[i]);
-    printf("%d", vetor[i]);
-    if(vetor[i] > vetor[i++]){
-        printf("O maior numero e' %d", vetor[i]);
+int main(){
+    int vetor[10];
+    int vlrMaior=0;
+    int vlrMenor=0;
+
+    for(int i=0; i< 10; i++){
+        printf("\nDigite o valor da posicao %d:", i);
+        scanf("%d", &vetor[i]);
+
+        if(i == 0){
+            vlrMenor = vetor[i];
+            vlrMaior = vetor[i];
+        }
+
+        if(vetor[i] > vlrMaior){
+            vlrMaior = vetor[i];
+        }
+
+        if(vetor[i] < vlrMenor){
+            vlrMenor = vetor[i];
+        }
+
     }
+
+    for(int i=0; i < 10; i++){
+        printf("\n%d\n", vetor[i]);
     }
-    return 0; 
-    }
+
+    printf("\n O valor maior he: %d", vlrMaior);
+    printf("\n O valor menor he: %d\n", vlrMenor);
+
+    return 0;
+}
